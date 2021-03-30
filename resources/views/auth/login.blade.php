ae9dad8e40bc7d514d251a20a7120073b64ebfbe
bastienbrj <brajon.bastien@gmail.com>
2021-03-30 16:20:08 +0200

    debug login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,44 +4,51 @@
             <img class="logo2" src="{{ asset('img/logo.png') }}">
         </x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+         <!-- Session Status -->
+         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="GET" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Adresse mail -->
-            <div>
-                <x-label for="utilisateurEmail" :value="__('Adresse mail')" />
-
-                <x-input id="utilisateurEmail" class="block mt-1 w-full" type="email" name="utilisateurEmail" :value="old('utilisateurEmail')" required autofocus />
-            </div>
-
-            <!-- Mot de passe -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Mot de passe')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
-                </label>
-            </div>
-            <div class="flex items-center justify-end mt-4">
-                <x-button class="ml-3">
-                    {{ __('Connexion') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+         <!-- Validation Errors -->
+         <x-auth-validation-errors class="mb-4" :errors="$errors" />
+ 
+         <form method="POST" action="{{ route('login') }}">
+             @csrf
+ 
+             <!-- Adresse mail -->
+             <div>
+                 <x-label for="utilisateurEmail" :value="__('Adresse mail')" />
+ 
+                 <x-input id="utilisateurEmail" class="block mt-1 w-full" type="email" name="utilisateurEmail" :value="old('utilisateurEmail')" required autofocus />
+             </div>
+ 
+             <!-- Mot de passe -->
+             <div class="mt-4">
+                 <x-label for="password" :value="__('Mot de passe')" />
+ 
+                 <x-input id="password" class="block mt-1 w-full"
+                                 type="password"
+                                 name="password"
+                                 required autocomplete="current-password" />
+             </div>
+ 
+             <!-- Remember Me -->
+             <div class="block mt-4">
+                 <label for="remember_me" class="inline-flex items-center">
+                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                     <span class="ml-2 text-sm text-gray-600">{{ __('Rester connecté') }}</span>
+                 </label>
+             </div>
+ 
+             <div class="flex items-center justify-end mt-4">
+                 @if (Route::has('password.request'))
+                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                         {{ __('Oublie du mot de passe ?') }}
+                     </a>
+                 @endif
+ 
+                 <x-button class="ml-3">
+                     {{ __('Connexion') }}
+                 </x-button>
+             </div>
+         </form>
+     </x-auth-card>
+ </x-guest-layout>
