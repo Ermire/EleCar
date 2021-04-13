5e9bc05ffb7e8b0a83ce714ba46e8a396ec6ef2f
bastienbrj <brajon.bastien@gmail.com>
2021-04-13 15:34:51 +0200

    /login

--- routes/auth.php
+++ routes/auth.php
@@ -11,11 +11,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [AuthenticatedSessionController::class, 'create'])
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
                 ->name('login');
 
-Route::post('/', [AuthenticatedSessionController::class, 'store'])
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
                 ->middleware('guest');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
