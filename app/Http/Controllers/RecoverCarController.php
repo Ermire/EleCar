5f97ff819305e34410fb7ebaa66db1532a90daa8
Etienne ROSSET <etienne.rosset@se.com>
2021-04-29 15:53:59 +0200

    oupsi

--- app/Http/Controllers/RecoverCarController.php
+++ app/Http/Controllers/RecoverCarController.php
@@ -7,9 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Car;
-use App\Models\Marque;
 
-class Controller extends BaseController
+class RecoverCarController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
