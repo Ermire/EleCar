f199c8f7b16c629184264b30f524fe50c2716fdf
bastienbrj <brajon.bastien@gmail.com>
2021-04-29 15:41:34 +0200

    page connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,27 +28,15 @@
                                  type="password"
                                  name="password"
                                  required autocomplete="current-password" />
-             </div>
- 
-             <!-- Remember Me -->
-             <div class="block mt-4">
-                 <label for="remember_me" class="inline-flex items-center">
-                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                     <span class="ml-2 text-sm text-gray-600">{{ __('Rester connecté') }}</span>
-                 </label>
-             </div>
- 
-             <div class="flex items-center justify-end mt-4">
-                 @if (Route::has('password.request'))
-                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                         {{ __('Mot de passe oublié ?') }}
-                     </a>
-                 @endif
- 
+            </div>   
+            <x-label>Pas encore inscrit ?<a class="linkRegister" href="{{ route('register') }}">
+                {{ __('S\'inscrire') }}
+            </a></x-label>
+             <div class="flex items-center justify-center mt-4">
                  <x-button class="ml-3">
                      {{ __('Connexion') }}
                  </x-button>
-             </div>
+                </div>
          </form>
      </x-auth-card>
  </x-guest-layout>
