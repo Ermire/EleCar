0ec565309df830f68a57b28f6a36b8d2055119f0
bastienbrj <brajon.bastien@gmail.com>
2021-04-30 15:28:43 +0200

    indentation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,39 +4,38 @@
             <img class="logo2" src="{{ asset('img/logo.png') }}">
         </x-slot>
 
-         <!-- Session Status -->
-         <x-auth-session-status class="mb-4" :status="session('status')" />
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-         <!-- Validation Errors -->
-         <x-auth-validation-errors class="mb-4" :errors="$errors" />
- 
-         <form method="POST" action="{{ route('login') }}">
-             @csrf
- 
-             <!-- Adresse mail -->
-             <div>
-                 <x-label for="utilisateurEmail" :value="__('Adresse mail')" />
- 
-                 <x-input id="utilisateurEmail" class="block mt-1 w-full" type="email" name="utilisateurEmail" :value="old('utilisateurEmail')" required autofocus />
-             </div>
- 
-             <!-- Mot de passe -->
-             <div class="mt-4">
-                 <x-label for="password" :value="__('Mot de passe')" />
- 
-                 <x-input id="password" class="block mt-1 w-full"
-                                 type="password"
-                                 name="password"
-                                 required autocomplete="current-password" />
-            </div>   
-            <x-label>Pas encore inscrit ?<a class="linkRegister" href="{{ route('register') }}">
-                {{ __('S\'inscrire') }}
-            </a></x-label>
-             <div class="flex items-center justify-center mt-4">
-                 <x-button class="ml-3">
-                     {{ __('Connexion') }}
-                 </x-button>
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!-- Adresse mail -->
+            <div>
+                <x-label for="utilisateurEmail" :value="__('Adresse mail')" />
+
+                <x-input id="utilisateurEmail" class="block mt-1 w-full" type="email" name="utilisateurEmail"
+                    :value="old('utilisateurEmail')" required autofocus />
             </div>
-         </form>
-     </x-auth-card>
- </x-guest-layout>
+
+            <!-- Mot de passe -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Mot de passe')" />
+
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
+            </div>
+            <x-label>Pas encore inscrit ?<a class="linkRegister" href="{{ route('register') }}">
+                    {{ __('S\'inscrire') }}
+                </a></x-label>
+            <div class="flex items-center justify-center mt-4">
+                <x-button class="ml-3">
+                    {{ __('Connexion') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Elecars') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Elecars') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset ('js/font_awesome_5.12.js') }}"></script>
-        <script src="{{ asset('js/jquery-3.5.1.slim.min.js')}}"></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/font_awesome_5.12.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
+</head>
 
-            </header>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+        <!-- Page Heading -->
+        <header class="bg-white shadow">
+
+        </header>
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
