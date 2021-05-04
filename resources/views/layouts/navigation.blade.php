00c97b9d943a1fb10daddd7c780f702b1f922c60
bastienbrj <brajon.bastien@gmail.com>
2021-05-04 20:11:19 +0200

    titre menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Tableau de bord') }}
+                        {{ __('Nos voitures') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
--- resources/views/offre.blade.php
+++ resources/views/offre.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 flex items-center justify-center">
-                    <p class="titre"><span class="bold">Nos offres de location !</span></p>
+                    <p class="titre"><span class="bold">Nos formules de location !</span></p>
                 </div>
             </div>
         </div>
