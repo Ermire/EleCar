b3a9a5ec4117c21582500ca8ce6fbec8b62be4f7
bastienbrj <brajon.bastien@gmail.com>
2021-05-05 11:54:55 +0200

    menu responsive

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,7 +71,13 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Tableau de bord') }}
+                {{ __('Nos voitures') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('offre')" :active="request()->routeIs('offre')">
+                {{ __('Nos offres') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
+                {{ __('Nous contacter') }}
             </x-responsive-nav-link>
         </div>
 
