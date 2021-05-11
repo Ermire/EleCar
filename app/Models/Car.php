b4408d659c9dd9df6019989344f705a0a03197e8
Etienne ROSSET <etienne.rosset@se.com>
2021-05-11 13:22:42 +0200

    prise en compte si la voiture est en location

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -17,6 +17,7 @@ class Car extends Model
             ->join('marque', 'vehicule.marque_IdMarque', '=', 'marque.marqueId')
             ->join('typevehicule', 'vehicule.typeVehicule_idtypeVehicule', '=', 'typevehicule.idtypeVehicule')
             ->join('classe', 'vehicule.classe_IdClasse', '=', 'classe.classeId')
+            ->where('voitureEnLocation', '=', 0)
             ->select('*');
         return $car->get();
     }
@@ -27,14 +28,17 @@ class Car extends Model
             ->join('marque', 'vehicule.marque_IdMarque', '=', 'marque.marqueId')
             ->join('typevehicule', 'vehicule.typeVehicule_idtypeVehicule', '=', 'typevehicule.idtypeVehicule')
             ->join('classe', 'vehicule.classe_IdClasse', '=', 'classe.classeId')
-            ->select('*')
-            ->where('vehiculeId', '=', $id);
+            ->where('vehiculeId', '=', $id)
+            ->select('*');
         return $car->get();
     }
 
     public function bookingCar($idUser, $idCar, $locStart, $locReturn, $assurance, $contrat)
     {
         DB::insert('insert into location (utilisateur_idUtilisateur, vehicule_idVehicule, locDateDepart, locDateRetour, locAssurance ) values (' . $idUser . ', ' . $idCar . ', "' . $locStart . '", "' . $locReturn . '", ' . $assurance . ')');
-        
+        DB::table('vehicule')
+        ->where('vehiculeId', $idCar)
+        ->update(['voitureEnLocation' => 1]);
     }
+    
 }
